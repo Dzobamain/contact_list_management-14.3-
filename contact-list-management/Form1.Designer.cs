fa12c3da417209ebb925b5ec4e48123ed53fd893
Dzoba Volodymyr <knopkayt26gmail.com>
2025-03-16 17:21:53 +0100

    Added the intended functionality to the program (#2)
    - Implemented fields for contact information.
    - Saving contacts to a file.
    - Search by parameters.
    - Loading from a file.

--- contact-list-management/Form1.Designer.cs
+++ contact-list-management/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +13,190 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.listBoxContacts = new System.Windows.Forms.ListBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.cmbCategoryFilter = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(12, 12);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.Text = "First Name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(12, 38);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 1;
+            this.txtLastName.Text = "Last Name";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 90);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.Text = "Email";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(12, 64);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 3;
+            this.txtPhone.Text = "Phone";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(118, 11);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategory.TabIndex = 4;
+            this.cmbCategory.Text = "Category";
+            // 
+            // listBoxContacts
+            // 
+            this.listBoxContacts.FormattingEnabled = true;
+            this.listBoxContacts.Location = new System.Drawing.Point(245, 38);
+            this.listBoxContacts.Name = "listBoxContacts";
+            this.listBoxContacts.Size = new System.Drawing.Size(543, 69);
+            this.listBoxContacts.TabIndex = 5;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(245, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(335, 20);
+            this.txtSearch.TabIndex = 6;
+            this.txtSearch.Text = "Search";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(164, 90);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(713, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(551, 113);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 9;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(470, 113);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(713, 113);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(632, 113);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 12;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // cmbCategoryFilter
+            // 
+            this.cmbCategoryFilter.FormattingEnabled = true;
+            this.cmbCategoryFilter.Location = new System.Drawing.Point(586, 11);
+            this.cmbCategoryFilter.Name = "cmbCategoryFilter";
+            this.cmbCategoryFilter.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategoryFilter.TabIndex = 13;
+            this.cmbCategoryFilter.Text = "Category Filter";
+            // 
+            // Form1
+            // 
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.cmbCategoryFilter);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.listBoxContacts);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Contact Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.ListBox listBoxContacts;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ComboBox cmbCategoryFilter;
     }
 }
-
--- contact-list-management/Form1.cs
+++ contact-list-management/Form1.cs
@@ -1,20 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace contact_list_management
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        private List<Contact> contacts = new List<Contact>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtFirstName.Text) &&
+                !string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                var contact = new Contact
+                {
+                    FirstName = txtFirstName.Text,
+                    LastName = txtLastName.Text,
+                    Phone = txtPhone.Text,
+                    Email = txtEmail.Text,
+                    Category = cmbCategory.Text
+                };
+                contacts.Add(contact);
+                UpdateContactList();
+                ClearInputFields();
+            }
+            else
+            {
+                MessageBox.Show("Введіть хоча б ім'я та телефон.", "Помилка");
+            }
+        }
+
+        private void UpdateContactList()
+        {
+            listBoxContacts.DataSource = null;
+            listBoxContacts.DataSource = contacts;
+            listBoxContacts.DisplayMember = "DisplayInfo";
+        }
+
+        private void ClearInputFields()
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtPhone.Text = "";
+            txtEmail.Text = "";
+            cmbCategory.SelectedIndex = -1;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var searchTerm = txtSearch.Text.ToLower();
+            var filteredContacts = contacts.Where(c => c.FirstName.ToLower().Contains(searchTerm)
+                                                    || c.Phone.Contains(searchTerm)).ToList();
+            listBoxContacts.DataSource = null;
+            listBoxContacts.DataSource = filteredContacts;
+            listBoxContacts.DisplayMember = "DisplayInfo";
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            var category = cmbCategoryFilter.Text;
+            var filteredContacts = contacts.Where(c => c.Category == category).ToList();
+            listBoxContacts.DataSource = null;
+            listBoxContacts.DataSource = filteredContacts;
+            listBoxContacts.DisplayMember = "DisplayInfo";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedItem is Contact selectedContact)
+            {
+                contacts.Remove(selectedContact);
+                UpdateContactList();
+            }
+            else
+            {
+                MessageBox.Show("Виберіть контакт для видалення.", "Помилка");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var writer = new StreamWriter("contacts.txt"))
+            {
+                foreach (var contact in contacts)
+                {
+                    writer.WriteLine($"{contact.FirstName},{contact.LastName},{contact.Phone},{contact.Email},{contact.Category}");
+                }
+            }
+            MessageBox.Show("Контакти успішно збережено.", "Успіх");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("contacts.txt"))
+            {
+                contacts.Clear();
+                var lines = File.ReadAllLines("contacts.txt");
+                foreach (var line in lines)
+                {
+                    var data = line.Split(',');
+                    var contact = new Contact
+                    {
+                        FirstName = data[0],
+                        LastName = data[1],
+                        Phone = data[2],
+                        Email = data[3],
+                        Category = data[4]
+                    };
+                    contacts.Add(contact);
+                }
+                UpdateContactList();
+                MessageBox.Show("Контакти успішно завантажено.", "Успіх");
+            }
+            else
+            {
+                MessageBox.Show("Файл із контактами не знайдено.", "Помилка");
+            }
+        }
+    }
+
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Category { get; set; }
+        public string DisplayInfo => $"{FirstName} {LastName} - {Phone}";
     }
 }
